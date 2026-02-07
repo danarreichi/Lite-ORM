9d2626856ef6bb9fe55ebe3a3b08d160c67d88ec
LEGION5I\danar <danar.ardiwinanto@gmail.com>
2026-02-07 19:07:17 +0700

    removed unused code

--- routes/index.js
+++ routes/index.js
@@ -2,35 +2,6 @@ const express = require('express');
 const router = express.Router();
 const homeController = require('../controllers/homeController');
 
-// Global variable to store current response object for debugging
-global._currentResponse = null;
-
-// Middleware to catch debug exceptions and provide response object
-const debugMiddleware = (req, res, next) => {
-  // Store current response object globally for dc() auto-detection
-  global._currentResponse = res;
-
-  // Override res.send to catch our debug exception
-  const originalSend = res.send;
-  res.send = function(data) {
-    // If this is called after dc(), don't send again
-    if (res.headersSent) {
-      return res;
-    }
-    return originalSend.call(this, data);
-  };
-
-  // Clean up after request
-  res.on('finish', () => {
-    global._currentResponse = null;
-  });
-
-  next();
-};
-
-// Apply debug middleware to all routes
-router.use(debugMiddleware);
-
 // Home page route
 router.get('/', (req, res) => {
   homeController.getHomePage(req, res);
